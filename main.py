a3102ff73e2b3e817b11607011a2965d4521f70a
f-leno <f.leno@usp.br>
2018-09-14 14:40:26 -0500

    Staging main.py

--- main.py
+++ main.py
@@ -48,3 +48,144 @@ def get_args():
     
     
     return parameter
+
+def build_agent(parameter):
+    """Builds and returns the agent object as specified by the arguments"""
+    
+    
+    #split in source, agent class
+    fileName,className = parameter.algorithm.split(":")
+    #Try to import agent class
+    try:
+        AgentClass = getattr(
+            __import__('agents.' + fileName,
+                      fromlist=[className]),
+                      className)
+    except ImportError as error:
+        print(error)
+        sys.stderr.write("ERROR importing python module: " +parameter.algorithm + "\n")
+        sys.exit(1)
+        
+    #If everything is OK, creates the object
+    agent = AgentClass()
+    
+    return agent
+
+def build_env(learningAgent,expert):
+    """ Builds and returns the Experiment Environment for learning"""
+    
+    parameter = get_args()
+    if parameter.train_env == "regular":
+        if parameter.stopping_criterion == 'episode':
+            trainingEnv = ExperimentCheckers(agentO = learningAgent, agentX = expert,totalEpisodes = parameter.interval,countFict = parameter.count_fict)
+        elif parameter.stopping_criterion == 'step':
+            trainingEnv = ExperimentCheckers(agentO = learningAgent, agentX = expert,totalSteps = parameter.interval,countFict = parameter.count_fict)
+    elif parameter.train_env in ["policy_pick","transfer_fs","transfer_fw"]:
+        if parameter.stopping_criterion == 'episode':
+            trainingEnv = ExperimentCheckers(learning = learningAgent, expert = expert,totalEpisodes = parameter.interval,countFict = parameter.count_fict, y=parameter.number_expert,x=parameter.number_simulated)
+        elif parameter.stopping_criterion == 'step':
+            trainingEnv = ExperimentCheckers(learning = learningAgent, expert = expert,totalSteps = parameter.interval,countFict = parameter.count_fict, y=parameter.number_expert,x=parameter.number_simulated)
+        
+    return trainingEnv
+
+def main():
+    parameter = get_args()
+    
+    #Change Seeds according to trial
+    Seeds().set_trial(parameter.trial)
+    
+    print("Starting Experiment:")
+    print(parameter)
+    #Creates a folder with the name of the algorithm
+    parameter.out += parameter.algorithm.split(":")[0] 
+    if parameter.count_fict:
+        parameter.out += "_countF" 
+    else:
+        parameter.out += "_noF"
+    if parameter.expert_optimality < 1.0:
+        parameter.out += "_" + str(parameter.expert_optimality)
+    
+    if parameter.train_env in ["transfer_fs","transfer_fw"]:
+        parameter.out += "_"+parameter.train_env 
+        
+    parameter.out +=  "/"
+    
+
+    
+    #Creates references for the agents
+    learningAgent = build_agent(parameter)
+    
+    if parameter.expert_optimality < 1.0 and parameter.train_env != "transfer_fs":
+        expertTrain = SuboptimalExpert(parameter.expert_optimality)
+    else:
+        expertTrain = ExpertCheckersAgent() 
+        
+    
+        
+    if parameter.train_env == "transfer_fw":
+        expertEval = ExpertCheckersAgent()
+    elif parameter.train_env == "transfer_fs":
+        expertEval = SuboptimalExpert(parameter.expert_optimality)
+    else:
+        expertEval = expertTrain
+        
+    
+   
+    
+    
+        
+    if parameter.recording_mode == 'reward':
+        recorder = DiscRewardExperimentRecorder(gamma=learningAgent.gamma, agentMarker = "O", 
+                                               interval = parameter.interval,resultFolder = parameter.out, trial = parameter.trial)
+    else:
+        recorder = None
+    #Creates the environments to be used for training and evaluation
+    #Those environment will run the learning process according to the time defined by the parameters
+    #of this source code
+    evalEnv = ExperimentCheckers(agentO = learningAgent, agentX = expertEval, totalEpisodes = parameter.duration, 
+                 experimentRecorder=recorder)#totalSteps = None, experimentRecorder = None, showGUI = False, storeVideo=False)
+    
+    
+    trainingEnv = build_env(learningAgent,expertTrain)
+    
+    
+    #Calculates how many times the learning experiment will be executed
+    training_sessions = int(parameter.learning_time / parameter.interval)
+    
+    
+    print("EVALUATION: " + str(0) + parameter.stopping_criterion)
+    #Initial evaluation with 0 training
+    learningAgent.exploring = False
+    #expert.exploring = False
+    evalEnv.run()
+    recorder.finish_experiment()
+    
+    
+    for i in range(training_sessions):
+        #First trains the agent, then evaluates
+        print("TRAINING: " + str(i*parameter.interval) + parameter.stopping_criterion)
+        learningAgent.exploring = True
+        #expert.exploring = True
+        trainingEnv.run()
+        print("EVALUATION: " + str((i+1)*parameter.interval) + parameter.stopping_criterion)
+        learningAgent.exploring = False
+        #expert.exploring = False
+        evalEnv.run()
+        recorder.finish_experiment()
+        
+    print("Finished")
+    recorder.end_learning()
+    
+
+
+        
+        
+        
+    
+    
+    
+    
+if __name__ == '__main__':
+    main()
+    
+ 
